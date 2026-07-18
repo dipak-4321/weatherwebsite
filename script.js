328a9119bbdd214cba03848a660b955b4953535e
dipak-4321 <dipakprince786@gmail.com>
2026-07-18 08:43:38 +0545

    weather icon has been working

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let buttonbox = document.querySelector(".search");
 let inputbox = document.querySelector(".header input");
-
+let weatherIcon = document.querySelector(".weather-icon");
 
 const url ="https://api.openweathermap.org/data/2.5/weather?&appid=fc0cf0dd82b93e46c264999f54371caa&units=metric&q=" ;
+
 async function checkWeather(city) {
     const response = await fetch( url + city );
     let data = await response.json();
@@ -13,6 +14,19 @@ async function checkWeather(city) {
     document.querySelector(".humidity").innerText = data.main.humidity + "%";
     document.querySelector(".wind").innerText = data.wind.speed + " km/h";
 
+    if(data.weather[0].main == "Clouds" ){
+        weatherIcon.src = "clouds.png";
+    } else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "Clear.png";
+    } else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "drizzle.png";
+    } else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "mist.png";
+    } else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "rain.png";
+    } else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "snow.png";
+    }
 
 }
 buttonbox.addEventListener("click", ()=>{
