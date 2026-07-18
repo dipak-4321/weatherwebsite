9040f4cf9bf826c8d578a7ad0c2a8d56ab02bff9
dipak-4321 <dipakprince786@gmail.com>
2026-07-18 08:31:17 +0545

    user input has been working

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const url ="https://api.openweathermap.org/data/2.5/weather?&appid=fc0cf0dd82b93e46c264999f54371caa&units=metric&q=kathmandu" ;
-async function checkWeather() {
-    const response = await fetch(url);
+let buttonbox = document.querySelector(".search");
+let inputbox = document.querySelector(".header input");
+
+
+const url ="https://api.openweathermap.org/data/2.5/weather?&appid=fc0cf0dd82b93e46c264999f54371caa&units=metric&q=" ;
+async function checkWeather(city) {
+    const response = await fetch( url + city );
     let data = await response.json();
     console.log(data);
 
-    
+    document.querySelector(".temp").innerText = data.main.temp;
     document.querySelector(".city").innerText = data.name;
     document.querySelector(".humidity").innerText = data.main.humidity + "%";
     document.querySelector(".wind").innerText = data.wind.speed + " km/h";
 
+
 }
-checkWeather();
+buttonbox.addEventListener("click", ()=>{
+   checkWeather(inputbox.value);
+});
